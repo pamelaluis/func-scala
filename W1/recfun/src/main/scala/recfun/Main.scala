79b70056de3df01984371d33adffb9eda1ada224
Pamela Luis <pamela.luis@gmail.com>
2013-10-04 18:55:45 -0700

    Assignment 1, Exercise 3 Change counting

--- W1/recfun/src/main/scala/recfun/Main.scala
+++ W1/recfun/src/main/scala/recfun/Main.scala
@@ -38,9 +38,15 @@ object Main {
     }
     balance_remaining(chars, 0)
   }
-
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0)
+      1
+    else if (money < 0 || coins.isEmpty)
+      0
+    else
+      countChange(money, coins.tail) + countChange(money - coins.head, coins)
+  }
 }
