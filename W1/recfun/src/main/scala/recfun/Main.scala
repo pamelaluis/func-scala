d58c23e4cfa0037c83beeeda03317702dc66c36d
Pamela Luis <pamela.luis@gmail.com>
2013-10-04 17:31:05 -0700

    Assignment 1, Exercise 2 Parentheses Balancing

--- W1/recfun/src/main/scala/recfun/Main.scala
+++ W1/recfun/src/main/scala/recfun/Main.scala
@@ -15,14 +15,29 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if ( c > r ) throw new IllegalArgumentException
-    if ( c == 0 || c == r) 1 else pascal(c-1,r-1) + pascal(c,r-1)
+    if (c > r) throw new IllegalArgumentException
+    if (c == 0 || c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
-//2,4 = 1,3 + 2,3
+  //2,4 = 1,3 + 2,3
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balance_remaining(chars: List[Char], openCount: Int): Boolean = {
+      if (chars.isEmpty)
+        openCount == 0
+      else if (openCount < 0)
+        false
+      else if (chars.head == '(')
+        balance_remaining(chars.tail, openCount + 1)
+      else if (chars.head == ')')
+        return balance_remaining(chars.tail, openCount - 1)
+      else
+        return balance_remaining(chars.tail, openCount)
+
+    }
+    balance_remaining(chars, 0)
+  }
 
   /**
    * Exercise 3
